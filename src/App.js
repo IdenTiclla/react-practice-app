191e0d3a960ed139037074b393741c0baed656b3
IdenTiclla <iden.ticlla@gmail.com>
2021-05-31 13:45:16 -0400

    add user list component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 
 import AddUser from './Components/Users/AddUser'
+import UsersList from './Components/Users/UsersList';
 
 function App() {
   return (
     <div>
         <AddUser/>
+        <UsersList users={[]}/>
     </div>
   );
 }
