523397a7b2cb9c389512a56562e609a04eb27fd6
IdenTiclla <iden.ticlla@gmail.com>
2021-05-31 14:20:17 -0400

    list of users via state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React, { useState } from 'react'
 import logo from './logo.svg';
 
 import AddUser from './Components/Users/AddUser'
 import UsersList from './Components/Users/UsersList';
 
 function App() {
-  return (
-    <div>
-        <AddUser/>
-        <UsersList users={[]}/>
-    </div>
-  );
+    const [usersList, setUsersList] = useState([])
+    const addUserHandler = (username, age) => {
+        setUsersList((prevUsersList)=> {
+            return [...prevUsersList, {name: username, age:age, id: Math.random().toString()}]
+        })
+    }
+    return (
+      <div>
+        <AddUser onAddUser={addUserHandler} />
+        <UsersList users={usersList} />
+      </div>
+    );
 }
 
 export default App;
